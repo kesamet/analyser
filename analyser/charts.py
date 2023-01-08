d3f8bb0596840f795d2219074386f7cb1cfaadda
kesamet <kesamet@gmail.com>
2023-01-08 13:30:06 +0800

    Update charts

--- analyser/charts.py
+++ analyser/charts.py
@@ -9,17 +9,9 @@ import pandas as pd
 import ta
 import altair as alt
 import streamlit as st
-from streamlit.elements import legacy_altair as altair
 
 from analyser.constants import str2days
-from analyser.utils_charts import (
-    get_data,
-    get_data_ohlcv,
-    get_ie_data,
-    get_pe_data,
-    pct_change,
-    rebase,
-)
+from analyser.utils_charts import get_data_ohlcv, pct_change
 
 try:
     from pm.config import DIRNAME, EQ_DICT
@@ -29,142 +21,6 @@ except ModuleNotFoundError:
         "MSCI ACWI": "ACWI",
         "MSCI World": "URTH",
     }
-
-
-def get_start_date(
-    today_date: datetime,
-    start_date="2015-01-01",
-    options=("YTD", "1M", "6M", "1Y", "2Y", "3Y", "All time"),
-) -> str:
-    select_range = st.selectbox("Select time range", options)
-    if select_range[-1] == "Y":
-        _yr = today_date.year - int(select_range[:-1])
-        return today_date.replace(year=_yr).strftime("%Y-%m-%d")
-    elif select_range[-1] == "M":
-        _mths = int(select_range[:-1])
-        return (today_date - timedelta(days=30 * _mths)).strftime("%Y-%m-%d")
-    elif select_range == "YTD":
-        return today_date.replace(month=1, day=1).strftime("%Y-%m-%d")
-    elif select_range == "MTD":
-        return today_date.replace(day=1).strftime("%Y-%m-%d")
-    return start_date
-
-
-@st.cache
-def load_pe_data(start_date="1990-01-01") -> pd.DataFrame:
-    """Shiller monthly PE data downloaded from quandl."""
-    return get_pe_data(f"{DIRNAME}/summary/pe_data.csv", start_date)
-
-
-@st.cache
-def load_ie_data(start_date="1871-01-01") -> pd.DataFrame:
-    """Data downloaded from http://www.econ.yale.edu/~shiller/data.htm."""
-    df = get_ie_data(f"{DIRNAME}/summary/ie_data.xls", start_date)
-    df["10xReal_Earnings"] = 10 * df["Real_Earnings"]
-    df["10xLong_IR"] = 10 * df["Long_IR"]
-    return df[["Real_Price", "10xReal_Earnings", "CAPE", "10xLong_IR"]]
-
-
-def _get_chart(start_date, dates, symbols, symbol_names=None, base_symbol="ES3.SI", title=""):
-    df = get_data(symbols, dates, base_symbol=base_symbol, dirname=DIRNAME)[symbols]
-    df1 = rebase(df[df.index >= start_date].copy())
-    if symbol_names is not None:
-        df1.columns = symbol_names
-    chart = altair.generate_chart("line", df1).properties(
-        title=title,
-        height=200,
-        width=260,
-    )
-    return chart
-
-
-def page_charts(today_date: datetime = date.today() - timedelta(days=1)) -> None:
-    start_date = get_start_date(today_date, options=("3Y", "2Y", "1Y"))
-    dates = pd.date_range(today_date - timedelta(days=365 * 2), today_date)
-
-    df0 = load_pe_data()
-    chart0 = altair.generate_chart("line", df0[["CAPE"]]).properties(
-        title="Shiller PE (CAPE) Plot",
-        height=200,
-        width=260,
-    )
-    st.altair_chart(chart0, use_container_width=True)
-
-    # df0 = load_ie_data()
-    # c1 = altair.generate_chart(
-    #     "line", df0[["Real_Price", "10xReal_Earnings"]]
-    # ).properties(
-    #     title="Index Plot",
-    #     height=200,
-    #     width=260,
-    # )
-    # c2 = altair.generate_chart("line", df0[["CAPE", "10xLong_IR"]]).properties(
-    #     title="PE (CAPE) Plot",
-    #     height=200,
-    #     width=260,
-    # )
-    # st.altair_chart(alt.concat(c1, c2, columns=2), use_container_width=True)
-
-    df1 = get_data(["^VIX"], dates, base_symbol="^VIX", dirname=DIRNAME)["^VIX"]
-    df1.columns = ["VIX"]
-    chart1 = altair.generate_chart("line", df1).properties(
-        title="VIX",
-        height=200,
-        width=260,
-    )
-    st.altair_chart(chart1, use_container_width=True)
-
-    st.subheader("Stock charts")
-    # MSCI
-    chart2 = _get_chart(
-        start_date,
-        dates,
-        ["URTH", "EEM", "SPY", "ES3.SI"],
-        symbol_names=["MSCI World", "MSCI EM", "S&P500", "ES3.SI"],
-        base_symbol="SPY",
-        title="MSCI",
-    )
-    st.altair_chart(chart2, use_container_width=True)
-
-    # ETFs
-    chart3 = _get_chart(
-        start_date,
-        dates,
-        ["IWDA.L", "EIMI.L"],
-        base_symbol="IWDA.L",
-        title="ETF",
-    )
-    st.altair_chart(chart3, use_container_width=True)
-
-    # banks
-    chart4 = _get_chart(
-        start_date,
-        dates,
-        ["ES3.SI", "D05.SI", "O39.SI", "U11.SI"],
-        symbol_names=["ES3", "DBS", "OCBC", "UOB"],
-        title="Banks",
-    )
-    st.altair_chart(chart4, use_container_width=True)
-
-    # industrial
-    chart5 = _get_chart(
-        start_date,
-        dates,
-        ["ES3.SI", "O5RU.SI", "A17U.SI", "BUOU.SI", "ME8U.SI", "M44U.SI"],
-        symbol_names=["ES3", "AA", "Ascendas", "FLCT", "MIT", "MLT"],
-        title="Industrial",
-    )
-    st.altair_chart(chart5, use_container_width=True)
-
-    # retail
-    chart6 = _get_chart(
-        start_date,
-        dates,
-        ["ES3.SI", "C38U.SI", "J69U.SI", "N2IU.SI"],
-        symbol_names=["ES3", "CICT", "FCT", "MPACT"],
-        title="Retail/Commercial",
-    )
-    st.altair_chart(chart6, use_container_width=True)
 
 
 @st.cache(allow_output_mutation=True)
